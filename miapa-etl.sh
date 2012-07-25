fe16e8d548af3ba59c13a245a9d8f1993bb59df3
Elliott Hauser <eah13@mac.com>
2012-07-25 19:38:27 -0300

    Script updated to reflect filename changes.

--- miapa-etl.sh
+++ miapa-etl.sh
@@ -35,12 +35,12 @@ ${one:="$HOME/git/miapa-etl/nexmlex/S10160.xml"}
 
 echo
 echo "  If your setup is like mine, your MIAPA ETL XSLT file is here:"
-echo "  $HOME/git/miapa-etl/configreader.xsl"
+echo "  $HOME/git/miapa-etl/nexml-isatab.xsl"
 echo
 echo "Enter absolute path of XSLT file to transform with or press enter to use above path:"
 read two
 
-${two:="$HOME/git/miapa-etl/configreader.xsl"}
+${two:="$HOME/git/miapa-etl/nexml-isatab.xsl"}
 
 echo
 echo "  If your setup is like mine, Saxon is here:"
